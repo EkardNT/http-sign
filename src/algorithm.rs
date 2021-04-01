ff35e1781197b3ecbc67ccff9b6441c5c93df116
Drake Tetreault <ekardnt@ekardnt.com>
2021-04-01 07:45:04 -0700

    Add several more signatures, including partial hs2019 support.

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,6 +1,4 @@
-use std::{fmt::Debug, io::Write};
-
-use ring::{rand::{SecureRandom}, signature::RsaKeyPair};
+use std::io::Write;
 
 /// The signature algorithm used to generate the HTTP message signature. The signature
 /// algorithm determines determines the hashing and signing algorithms used in computing
@@ -17,62 +15,226 @@ pub trait SignatureAlgorithm {
     /// Is the (created) signature element allowed?
     fn allows_created(&self) -> bool;
 
-    /// Hash a block of data.
-    fn hash(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()>;
-
     /// Digitally sign a block of data.
     fn sign(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()>;
 }
 
-pub struct RsaSha256<Rand> {
-    key_id: String,
-    key: RsaKeyPair,
-    random: Rand,
-}
+mod hs {
+    use std::io::Write;
 
-impl<Rand> RsaSha256<Rand> {
-    pub fn new(key_id: impl Into<String>, key: RsaKeyPair, random: Rand) -> Self {
-        Self {
-            key_id: key_id.into(),
-            key,
-            random
+    use ring::{rand::SecureRandom, signature::{RsaEncoding, RsaKeyPair}};
+
+    use super::SignatureAlgorithm;
+
+    pub enum Hs2019<Rand> {
+        Rsa {
+            padding_alg: &'static dyn RsaEncoding,
+            key_id: String,
+            key: RsaKeyPair,
+            random: Rand
+        }
+    }
+
+    impl<Rand: SecureRandom> SignatureAlgorithm for Hs2019<Rand> {
+        fn name(&self) -> &str {
+            "hs2019"
+        }
+
+        fn key_id(&self) -> &str {
+            match self {
+                Hs2019::Rsa { key_id, .. } => key_id
+            }
+        }
+
+        fn allows_created(&self) -> bool {
+            true
+        }
+
+        fn sign(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()> {
+            match self {
+                Hs2019::Rsa { padding_alg, key, random, .. } => {
+                    // 1024 bytes is enough for RSA-8192 keys.
+                    let mut signature = [0u8; 1024];
+                    let signature = &mut signature[..key.public_modulus_len()];
+                    key.sign(*padding_alg, random, data, signature)
+                        .expect("Failed to compute RSA signature");
+                    output.write_all(signature)
+                }
+            }
+
         }
     }
 }
 
-impl<Rand: SecureRandom> SignatureAlgorithm for RsaSha256<Rand> {
-    fn name(&self) -> &str {
-        "rsa-sha256"
+mod rsa {
+    use std::fmt::Debug;
+    use std::io::Write;
+
+    use ring::{rand::SecureRandom, signature::RsaKeyPair};
+
+    use super::SignatureAlgorithm;
+
+    #[deprecated]
+    pub struct RsaSha256<Rand> {
+        key_id: String,
+        key: RsaKeyPair,
+        random: Rand,
     }
 
-    fn key_id(&self) -> &str {
-        &self.key_id
+    impl<Rand> RsaSha256<Rand> {
+        pub fn new(key_id: impl Into<String>, key: RsaKeyPair, random: Rand) -> Self {
+            Self {
+                key_id: key_id.into(),
+                key,
+                random
+            }
+        }
     }
 
-    fn allows_created(&self) -> bool {
-        false
+    impl<Rand: SecureRandom> SignatureAlgorithm for RsaSha256<Rand> {
+        fn name(&self) -> &str {
+            "rsa-sha256"
+        }
+
+        fn key_id(&self) -> &str {
+            &self.key_id
+        }
+
+        fn allows_created(&self) -> bool {
+            false
+        }
+
+        fn sign(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()> {
+            // 1024 bytes is enough for RSA-8192 keys.
+            let mut signature = [0u8; 1024];
+            let signature = &mut signature[..self.key.public_modulus_len()];
+            self.key.sign(&ring::signature::RSA_PKCS1_SHA256, &self.random, data, signature)
+                .expect("Failed to compute RSA_PKCS1_SHA256");
+            output.write_all(signature)
+        }
     }
 
-    fn hash(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()> {
-        let digest = ring::digest::digest(&ring::digest::SHA1_FOR_LEGACY_USE_ONLY, data);
-        output.write_all(digest.as_ref())
-    }
-
-    fn sign(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()> {
-        // 1024 bytes is enough for RSA-8192 keys.
-        let mut signature = [0u8; 1024];
-        let signature = &mut signature[..self.key.public_modulus_len()];
-        self.key.sign(&ring::signature::RSA_PKCS1_SHA256, &self.random, data, signature)
-            .expect("Failed to compute RSA_PKCS1_SHA256");
-        output.write_all(signature)
+    impl<Rand> Debug for RsaSha256<Rand> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("RsaSha256")
+                .field("key_id", &self.key_id)
+                .field("key", &self.key)
+                .finish()
+        }
     }
 }
 
-impl<Rand> Debug for RsaSha256<Rand> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("RsaSha256")
-            .field("key_id", &self.key_id)
-            .field("key", &self.key)
-            .finish()
+mod hmac {
+    use std::fmt::Debug;
+    use std::io::Write;
+
+    use ring::{rand::SecureRandom, hmac::Key};
+
+    use super::SignatureAlgorithm;
+
+    #[deprecated]
+    pub struct HmacSha256 {
+        key_id: String,
+        key: Key,
     }
+
+    impl HmacSha256 {
+        /// Constructs a new `HmacSha256` from the supplied HMAC Key. Panics if the Key's
+        /// algorithm is not HMAC_SHA256.
+        pub fn new(key_id: impl Into<String>, key: Key) -> Self {
+            assert_eq!(ring::hmac::HMAC_SHA256, key.algorithm(),
+                "HMAC key algorithm should have been {:?} but was {:?}", ring::hmac::HMAC_SHA256, key.algorithm());
+            Self {
+                key_id: key_id.into(),
+                key,
+            }
+        }
+    }
+
+    impl SignatureAlgorithm for HmacSha256 {
+        fn name(&self) -> &str {
+            "rsa-sha256"
+        }
+
+        fn key_id(&self) -> &str {
+            &self.key_id
+        }
+
+        fn allows_created(&self) -> bool {
+            false
+        }
+
+        fn sign(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()> {
+            let tag = ring::hmac::sign(&self.key, data);
+            output.write_all(tag.as_ref())
+        }
+    }
+
+    impl Debug for HmacSha256 {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("HmacSha256")
+                .field("key_id", &self.key_id)
+                .field("key", &self.key)
+                .finish()
+        }
+    }
+}
+
+mod ecdsa {
+    use std::fmt::Debug;
+    use std::io::Write;
+
+    use ring::{rand::SecureRandom, signature::EcdsaKeyPair};
+
+    use super::SignatureAlgorithm;
+
+    #[deprecated]
+    pub struct EcdsaSha256<Rand> {
+        key_id: String,
+        key: EcdsaKeyPair,
+        random: Rand,
+    }
+
+    impl<Rand> EcdsaSha256<Rand> {
+        /// Construct a new EcdsaSha256 from the specified key. The key should of
+        /// algorithm ECDSA_P256_SHA256_ASN1_SIGNING, but there is no way to verify that
+        /// from ring's current API.
+        pub fn new(key_id: impl Into<String>, key: EcdsaKeyPair, random: Rand) -> Self {
+            Self {
+                key_id: key_id.into(),
+                key,
+                random,
+            }
+        }
+    }
+
+    impl<Rand: SecureRandom> SignatureAlgorithm for EcdsaSha256<Rand> {
+        fn name(&self) -> &str {
+            "ecdsa-sha256"
+        }
+
+        fn key_id(&self) -> &str {
+            &self.key_id
+        }
+
+        fn allows_created(&self) -> bool {
+            false
+        }
+
+        fn sign(&self, data: &[u8], output: &mut dyn Write) -> std::io::Result<()> {
+            let signature = self.key.sign(&self.random, data)
+                .expect("Failed to sign message in EcdsaSha256");
+            output.write_all(signature.as_ref())
+        }
+    }
+
+    impl<Rand> Debug for EcdsaSha256<Rand> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("EcdsaSha256")
+                .field("key_id", &self.key_id)
+                .field("key", &self.key)
+                .finish()
+        }
+    }
+
 }
